59c1592e0dc4254a91771c9392a19e6b0864ae89
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-03-12 12:24:48 +0200

    добавляє мобільне меню

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,34 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+const refs = {
+  openMenu: document.querySelector('.open-menu'),
+  closeMenu: document.querySelector('.close-menu'),
+  modal: document.querySelector('.modal-menu'),
+  list: document.querySelector('.modal-menu__list')
+};
+
+const menuToggle = () => {
+  refs.modal.classList.toggle('hidden');
+  document.addEventListener('keydown', escCloseModal);
+};
+
+const modalMenuClose = e => {
+  if (e.target.className === 'backdrop') {
+    menuToggle();
+    document.removeEventListener('keydown', escCloseModal);
+  }
+};
+
+const escCloseModal = e => {
+    if ((e.key === "Escape")) {
+    menuToggle();
+document.removeEventListener('keydown', escCloseModal);
+  }
+};
+
+refs.openMenu.addEventListener('click', menuToggle);
+
+refs.closeMenu.addEventListener('click', menuToggle);
+
+refs.modal.addEventListener('click', modalMenuClose);
+
+refs.list.addEventListener('click', menuToggle)
+
